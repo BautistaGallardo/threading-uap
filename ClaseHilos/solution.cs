e06ba963bcc5ae86bd403c7467e913b85fd6de44
Bautista Gallardo <bautistaggallardo@gmail.com>
2024-06-11 01:03:39 -0300

    Entrega guia 11 Gallardo Bautista y Idiarte Joaquin

--- ClaseHilos/solution.cs
+++ ClaseHilos/solution.cs
@@ -1,22 +1,23 @@
 namespace ClaseHilos
 {
-   internal class Producto
-   {
-      public string Nombre { get; set; }
-      public decimal PrecioUnitarioDolares { get; set; }
-      public int CantidadEnStock { get; set; }
+    internal class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal PrecioUnitarioDolares { get; set; }
+        public int CantidadEnStock { get; set; }
 
-      public Producto(string nombre, decimal precioUnitario, int cantidadEnStock)
-      {
-         Nombre = nombre;
-         PrecioUnitarioDolares = precioUnitario;
-         CantidadEnStock = cantidadEnStock;
-      }
-   }
-   internal class Solution //reference: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/lock
-   {
+        public Producto(string nombre, decimal precioUnitario, int cantidadEnStock)
+        {
+            Nombre = nombre;
+            PrecioUnitarioDolares = precioUnitario;
+            CantidadEnStock = cantidadEnStock;
+        }
+    }
+    internal class Solution //reference: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/lock
+    {
+        // integrantes del grupo: Gallardo Bautista y Idiarte Joaquín
 
-      static List<Producto> productos = new List<Producto>
+        static List<Producto> productos = new List<Producto>
         {
             new Producto("Camisa", 10, 50),
             new Producto("Pantalón", 8, 30),
@@ -25,24 +26,74 @@ namespace ClaseHilos
             new Producto("Gorra", 16, 10)
         };
 
-      static int precio_dolar = 500;
+        static int precio_dolar = 500;
 
-      static void Tarea1()
-      {
-         throw new NotImplementedException();
-      }
-      static void Tarea2()
-      {
-         throw new NotImplementedException();
-      }
-      static void Tarea3()
-      {
-         throw new NotImplementedException();
-      }
+        // Declaramos el semaforo binario
+        static new Mutex mutex = new Mutex();
 
-      internal static void Excecute()
-      {
-         throw new NotImplementedException();
-      }
-   }
+        // Declaramos barrera
+        static new Barrier barrera = new Barrier(2, (b) =>
+        {
+            Console.WriteLine($"Post-Phase action: {b.CurrentPhaseNumber}");
+            Tarea3();
+        });
+
+        // Actualizacion de stock
+        static void Tarea1()
+        {
+            // Entra a la region critica
+            mutex.WaitOne();
+            Console.WriteLine("Agregando productos");
+            foreach (var producto in productos)
+            {
+                producto.CantidadEnStock += 10;
+            }
+            Console.WriteLine("Productos agregados");
+            // Sale de la region critica
+            mutex.ReleaseMutex();
+            // Espera a que el otro hilo llegue si es que no llego
+            barrera.SignalAndWait();
+        }
+        static void Tarea2()
+        {
+            // Entra a la region critica
+            mutex.WaitOne();
+            Console.WriteLine("Agregando precios");
+            precio_dolar = 1280;
+            Console.WriteLine("Precios agregaos");
+            // Sale de la region critica
+            mutex.ReleaseMutex();
+            // Espera a que el otro hilo llegue si es que no llego
+            barrera.SignalAndWait();
+        }
+        static void Tarea3()
+        {
+            // pasamos dolares a pesos
+            Console.WriteLine("Listando productos");
+            foreach (var producto in productos)
+            {
+                producto.PrecioUnitarioDolares = producto.PrecioUnitarioDolares * precio_dolar;
+            }
+
+            // generamos informe de la actualizacion de precios y stock
+            Console.WriteLine("Productos actualizados");
+            foreach (var producto in productos)
+            {
+                Console.WriteLine($"Nombre: {producto.Nombre}, Precio: {producto.PrecioUnitarioDolares}, Stock: {producto.CantidadEnStock}");
+            }
+            Console.ReadLine();
+        }
+
+        internal static void Excecute()
+        {
+            // instanciamos los hilos
+            Thread task1 = new Thread(Tarea1);
+            Thread task2 = new Thread(Tarea2);
+            task1.Start();
+            task2.Start();
+
+
+
+        }
+    }
 }
